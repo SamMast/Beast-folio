1c90db65d031255ae24dce60bd841de640dbd518
Samuel Mast <samuel.a.mast@gmail.com>
2018-04-04 11:20:50 -0600

    update to new database for heroku

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import { Link } from "react-router-dom";
 import Nav from "../components/Nav";
+import "./Home.css";
 
 class Home extends Component {
 
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://sam:mast@ds131119.mlab.com:31119/heroku_kd1r0hmc",
+  process.env.MONGODB_URI || "mongodb://sam:mast@ds235239.mlab.com:35239/heroku_0p1v4fck",
   {
     useMongoClient: true
   }
